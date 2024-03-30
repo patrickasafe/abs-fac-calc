38221d5dfe2a31c00cea97f4daf850bbd0b0df93
patrickasafe <patrickasafe@gmail.com>
2024-03-30 18:40:42 +0000

    chore: update page info

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
-  title: 'Next.js + Tailwind CSS + TypeScript Starter',
+  title: 'Calculadora de Fator e Absorbância',
   description:
-    'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
+    'Uma aplicação voltada para auxiliar o cálculo de calibração em aparelhos que utilizam absorbância ou fator',
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  url: 'https://tsnext-tw.thcl.dev',
+  url: 'https://xtecalc.vercel.app/',
 };
